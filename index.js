f2814dba0fa5951e169a88cb9a0900a54dfc552d
froehlichA <froald14@new.edvhtl.at>
2017-05-20 10:38:38 +0200

    [Buggy] Added announcement.
    Code buggy and unfinished, but not throwing errors.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var name = "ndBot";
-var prefix = "$$";
+var pre = "$$";
 
-//Guide to Log-Messages
+// -- GUIDE TO LOG-MESSAGES --
 //INFO -- <Message>        for info and general statements
 //WARNING -- <Message>     for warnings that have little to no impact
 //ERROR -- <Message>       Exceptions and errors with medium impact
 //CRITICAL -- <Message>    for critical Errors that could break the bot
+
 
 // -- TODO --
 //Permission checking not yet implemented.
@@ -17,6 +18,49 @@ var prefix = "$$";
 //Announcement not working, but no error in console.
 
 client.on('ready', () => { console.log('INFO -- '+name+' ready.'); });
+
+client.on('message', message => {
+	var arr = message.content.split(" ");
+	var command = arr[0];
+
+	//Handler for GENERAL HELP
+	if(command == pre+"help") {
+		showGeneralHelp(message);
+		return;
+	}
+
+	//Handler for ANNOUNCE
+	if(command == pre+"announce") {
+		if(arr.length==1) showHelpForAnnouncement(message);
+		if(arr.length>1)  doAnnouncement(message);
+	}
+});
+
+
+
+
+
+//Shows the general help for the bot.
+function showGeneralHelp(message) {
+	message.channel.send("GENERAL HELP");
+}
+
+//Shows the help for the announcement
+function showHelpForAnnouncement(message) {
+	message.channel.send("ANNOUNCEMENT HELP");
+}
+
+//Does the announcement.
+function doAnnouncement(message) {
+	if(!checkOnServer(message)) return;
+
+	for(var chan of message.guild.channels) {
+		console.log("Channels: " + chan.type);
+		if(chan.type=="text") {
+			chan.send("ANNOUCEMENT: <Missing Text>");
+		}
+	}
+}
 
 //Checks if this message was sent on server, and server is available.
 function checkOnServer(message) {
@@ -31,5 +75,6 @@ function checkOnServer(message) {
 	}
 	return true;
 }
+
 //LOGIN -- Always at end of file
 client.login(process.argv[2]);
