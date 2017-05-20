19d2aa3b74bf6fd3df4e1db1dde5e526b656a718
Fröhlich A <froehlich.alexander@protonmail.com>
2017-05-20 20:53:45 +0200

    Cleared up Help Page.
    Final clean up of help page. Branch now ready to be merged.

--- c_help.js
+++ c_help.js
@@ -9,7 +9,7 @@ function showGeneralHelp(message, Discord, client) {
 		.setFooter('Thank you for using KeanBot!')
 		.setThumbnail(client.user.avatarURL)
 		//Help Messages
-		.addField(':loudspeaker: $$announce', 'Writes a message to every channel.')
+		.addField(':loudspeaker: $$announce <Message>', 'Writes a message to every channel.')
 		.addField(':busts_in_silhouette: $$stats', 'Displays the stats of the user.');
 	message.channel.send({embed});
 }
