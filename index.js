585ebd10548b33654a165b621cb3d6d43196a163
Fröhlich A <froehlich.alexander@protonmail.com>
2017-05-20 09:24:26 +0200

    Added output description.
    Description for Log-Messages and Guide how to use them.

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const client = new Discord.Client();
 var name = "ndBot";
 var prefix = "$$";
 
-client.on('ready', () => { console.log(name+' ready.'); });
+//Guide to Log-Messages
+//INFO -- <Message>        for info and general statements
+//WARNING -- <Message>     for warnings that have little to no impact
+//ERROR -- <Message>       Exceptions and errors with medium impact
+//CRITICAL -- <Message>    for critical Errors that could break the bot
+
+client.on('ready', () => { console.log('INFO -- '+name+' ready.'); });
 
 //LOGIN -- Always at end of file
 client.login(process.argv[2]);
