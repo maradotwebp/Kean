4300c722e304c8a0bcd6b0222145601f9b44b746
Fröhlich A <froehlich.alexander@protonmail.com>
2017-05-20 20:47:38 +0200

    Changed color.
    The bot now is only one color.

--- c_help.js
+++ c_help.js
@@ -4,7 +4,7 @@
 function showGeneralHelp(message, Discord, client) {
 	const embed = new Discord.RichEmbed()
 		.setTitle(':tools:    -- Kean General Help --   :tools:')
-		.setColor(0x00AE86)
+		.setColor([188, 123, 55])
 		.setDescription("The Bot for everything and everything else!")
 		.setFooter('Thank you for using KeanBot!')
 		.setThumbnail(client.user.avatarURL)
