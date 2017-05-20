2d45e587db14fecdf87bf0d3ca64e7a16d00b0d3
froehlichA <froald14@new.edvhtl.at>
2017-05-20 17:53:03 +0200

    Code now cleaned up.
    Code now in seperate files, and everything is working correctly. Easier to look at.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+var Command = require('./command.js');
 const client = new Discord.Client();
 var name = "ndBot";
 var pre = "$$";
@@ -16,80 +17,19 @@ var pre = "$$";
 //Change help for annoucement.
 //Change general help.
 
+//On start, logs and sets game to help info.
 client.on('ready', () => {
 	console.log('INFO -- '+name+' ready.');
 	client.user.setGame("with $$help!");
 });
 
-
-
+//Delegates the command to command.js
 client.on('message', message => {
-	var arr = message.content.split(" ");
-	var command = arr[0];
-
-	//Handler for GENERAL HELP
-	if(command == pre+"help" || command == pre) {
-		showGeneralHelp(message);
-		return;
-	}
-	//Handler for ANNOUNCE
-	else if(command == pre+"announce") {
-		if(arr.length==1) showHelpForAnnouncement(message);
-		if(arr.length>1)  doAnnouncement(message);
-	}
-	else if(command.startsWith(pre)) {
-		notSupported(message);
+	if(message.author.bot) return;
+	if(message.content.startsWith("$$")) {
+		Command.commandDelegate(message,pre, Discord);
 	}
 });
-
-
-
-
-
-//Shows the general help for the bot.
-function showGeneralHelp(message) {
-	message.channel.send(":no_entry: -- **Moderation Tools**");
-	message.channel.send(" $$announce");
-}
-
-//Shows the help for the announcement
-function showHelpForAnnouncement(message) {
-	message.channel.send(":loudspeaker: -- **$$announce** -- :loudspeaker:");
-	message.channel.send("**Usage:** $$announce <Text>");
-	message.channel.send("**Desc:** Writes a text to every Channel in the Guild.");
-}
-
-//Does the announcement.
-function doAnnouncement(message) {
-	if(!checkOnServer(message)) return;
-
-	for(var chan of message.guild.channels) {
-		var text = message.content.replace(pre+"announce ", "");
-		if(chan[1].type=="text") {
-			chan[1].send(":loudspeaker: "+text+" :loudspeaker:");
-		}
-	}
-}
-
-//Checks if this message was sent on server, and server is available.
-function checkOnServer(message) {
-	var guild = message.guild;
-	if(!(guild instanceof Discord.Guild)) {
-		message.channel.send("I'm sorry, you are not on a Server!");
-		return false;
-
-	}
-	if(!guild.available) {
-		message.channel.send("Sorry, the guild seems to be unavailable!");
-		return false;
-	}
-	return true;
-}
-
-function notSupported(message) {
-	message.channel.send(":confused: I'm sorry, I didn't get that.");
-	message.channel.send("Use **$$help** if you want some information.");
-}
 
 //LOGIN -- Always at end of file
 client.login(process.argv[2]);
