7ac082d2a7f21b7dc3034bd5434649e4048de42c
Fröhlich A <froehlich.alexander@protonmail.com>
2017-05-24 10:05:41 +0200

    Renamed client to bot to avoid confusion.
    client is now named bot.

--- c_stats.js
+++ c_stats.js
@@ -2,14 +2,14 @@ const Discord = require('discord.js');
 
 
 //Sends the stats of the bot to the channel.
-function showStats(client, channel, name) {
+function showStats(bot, channel, name) {
     const embed = new Discord.RichEmbed()
         .setTitle(name+' Statistics')
         .setColor([188, 123, 55])
         .addField('Mem Usage', `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
-        .addField('Users', `${client.users.size}`, true)
-        .addField('Servers', `${client.guilds.size}`, true)
-        .addField('Channels', `${client.channels.size}`, true)
+        .addField('Users', `${bot.users.size}`, true)
+        .addField('Servers', `${bot.guilds.size}`, true)
+        .addField('Channels', `${bot.channels.size}`, true)
         .addField('Version', `0.0.6`, true)
         .addField('Discord.js', `v${Discord.version}`, true);
     channel.send({embed});
