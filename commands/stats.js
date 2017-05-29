2d2d4911b383784f5d3e5bc0afef3d4530f54de8
froehlichA <froald14@new.edvhtl.at>
2017-05-29 12:37:15 +0200

    Added all commands.
    Commands now added and rewritten.

---
+++ commands/stats.js
@@ -0,0 +1,20 @@
+exports.run = (bot, message, args) => {
+	let Discord = require('discord.js');
+	let config = require('../config/config.json');
+
+	const embed = new Discord.RichEmbed()
+        .setTitle(`${config.name}Bot Statistics`)
+        .setDescription("Warning: Information might be slightly inaccurate.")
+        .setColor([188, 123, 55])
+        .addField('Mem Usage', `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
+        .addField('Users', `${bot.users.size}`, true)
+        .addField('Servers', `${bot.guilds.size}`, true)
+        .addField('Channels', `${bot.channels.size}`, true)
+        .addField('Version', `0.6`, true)
+        .addField('Discord.js', `v${Discord.version}`, true);
+    message.channel.send({embed});
+}
+
+exports.help = () => {
+	return [":satellite:", "stats", "Displays the stats of the bot."];
+}
