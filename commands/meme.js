d6162ec1fc0d8be643d5aa53d983495725db9ce2
Unknown <sklphd14@htlkaindorf.at>
2017-06-01 13:25:51 +0200

    Fixed some things.

--- commands/meme.js
+++ commands/meme.js
@@ -2,20 +2,21 @@ exports.run = (bot, message, args) =>
 {
   let Discord = require('discord.js');
   const fs = require('fs');
+  var lwip = require('lwip');
 
   var random;
-  fs.readdir(`./Memes/${args[0]}/`, (err, files) => {
+  fs.readdir(`./Memes/${args[0]}/`, (err, files) =>
+  {
     random = Math.random() * (files.length - 1) + 1;
   });
 
-  fs.readFile(`./Memes/${args[0]}/${random}.jpeg`, (err, data) =>
+  lwip.open(`./Memes/${args[0]}/${random}.jpg`, function(err, image)
   {
       if(err) throw err;
+      message.channel.send(image);
+  });
 
-  }
-
-
- }
+}
 
 
 
